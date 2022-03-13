9190353031029b2e1842eccd7fa232742d93f68a
Davida149 <adegundedavid17@gmail.com>
2022-03-13 08:48:35 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+/**
+ *main - print all single numbers of base 10 starting from 0
+ *
+ *Return: Alawys 0 (Success)
+ */
+int main(void)
+{
+int i;
+
+for (i = 48; i < 58; i++)
+	{
+putchar(i);
+	}
+
+putchar('\n');
+return (0);
+
+}
