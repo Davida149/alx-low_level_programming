9533f44948c238d2ceca0ecf9e0f8085168e42d8
Davida149 <adegundedavid17@gmail.com>
2022-03-18 22:34:01 +0100

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *print_numbers - A function that prints numbers from 0-9
+ *integer to be printed
+ *Return: Alawys 0
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		_putchar('i');
+	}
+	putchar('\n');
+	return (0);
+}
