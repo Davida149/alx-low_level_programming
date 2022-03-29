0fbe6f6593e0d89946e94e6b19c34f2ab6919cc0
Davida149 <adegundedavid17@gmail.com>
2022-03-29 23:22:35 +0100

    3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -2,7 +2,7 @@
 /**
  *_strspn - gets the length of a prefix substring
  *@s: segment to compare bytes from
- *@accept:string of bytes to compare with
+ *@accept: string of bytes to compare with
  *Return: number of bytes in segment s which consist of bytes from accept
  */
 unsigned int _strspn(char *s, char *accept)
