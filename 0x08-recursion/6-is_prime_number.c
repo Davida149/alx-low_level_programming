62e44bf7eb9e278134ff1a12305890fb6bb8fd14
Davida149 <adegundedavid17@gmail.com>
2022-03-31 23:16:55 +0100

    6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -28,13 +28,9 @@ int _prime_check(int i, int k)
  */
 int is_prime_number(int n)
 {
-	if (n == 0)
+	if (n <= 1)
 	{
 		return (0);
-	}
-	else if (n >= 1)
-	{
-		return (1);
 	}
 	return (_prime_check(n, 2));
 }
