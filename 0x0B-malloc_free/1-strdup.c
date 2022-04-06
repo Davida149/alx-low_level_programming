e1429df5f4c452994dab76d347fb1c4b888a0e5b
Davida149 <adegundedavid17@gmail.com>
2022-04-06 23:17:28 +0100

    1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -18,7 +18,7 @@ char *_strdup(char *str)
 	while (*(str + i) != '\0')
 		i++;
 	i++;
-	ptr = (char *)malloc(i + sizeof(char));
+	ptr = (char *)malloc(i * sizeof(char));
 	if (ptr == NULL)
 		return (NULL);
 	for (j = 0; *(str + i) != '\0'; j++)
