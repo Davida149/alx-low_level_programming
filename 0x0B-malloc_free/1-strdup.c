9e5b261c2cf67f42e6e787bf821dbb0e1061615e
Davida149 <adegundedavid17@gmail.com>
2022-04-06 23:11:32 +0100

    1-strdup.c

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ *_strdup - duplicates strings specified
+ *@str: pointer to string
+ *
+ *
+ *Return: ptr
+ */
+char *_strdup(char *str)
+{
+	unsigned int i = 0, j;
+
+	char *ptr
+
+	if (str == NULL)
+		return (NULL);
+	while (*(str + i) != '\0')
+		i++
+	i++;
+	ptr = (char *)malloc(i + sizeof(char));
+	if (ptr == NULL)
+		return (NULL);
+	for (j = 0; *(str + i) != '\0'; j++)
+	{
+		*(ptr + j) = *(str + j);
+	}
+	return (ptr);
+}
