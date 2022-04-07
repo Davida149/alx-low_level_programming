e4486041488ae52ecb53de5b5bcd20e526e54b61
Davida149 <adegundedavid17@gmail.com>
2022-04-08 00:38:14 +0100

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -9,41 +9,24 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *ptr;
-
-	int i, len1 = 1, len2 = 1, tot_len, k, l, j;
+	char *p;
+	unsigned int s1count, s2count, sizeBuffer, i;
 
 	if (s1 == NULL)
 	s1 = "";
 	if (s2 == NULL)
 	s2 = "";
-	for (i = 0; *(s1 + i) != '\0'; i++)
-	{
-		len1 = len1 + i;
-	}
-	for (i = 0; i <= n; i++)
-	{
-		len2 = len2 + i;
-	}
-	tot_len = len1 + len2 + 1;
-
-
-	ptr = malloc(sizeof(char) * tot_len);
-	if (ptr == NULL)
-		return (NULL);
-	for (j = 0; j < tot_len; j++)
-	{
-		if (*(s1 + k) != '\0')
-		{
-			*(ptr + j) = *(s1 + k);
-			k++;
-		}
-		else if (*(s1 + k) == '\0' && *(s2 + l) != '\0')
-		{
-			*(ptr + j) = *(s2 + l);
-			l++;
-		}
-		*(ptr + j) = '\0';
-	}
-	return (ptr);
+	for (s1count = 0; s1[s1count]; s1count++)
+	;
+	for (s2count = 0; s2[s2count]; s2count++)
+	;
+	s2count > n ? (s2count = n) : (n = s2count);
+	sizeBuffer = s1count + s2count + 1;
+	p = malloc(sizeBuffer * sizeof(char));
+	if (p == NULL)
+	return (NULL);
+	for (i = 0; i < sizeBuffer - 1; i++)
+	i < s1count ? (p[i] = s1[i]) : (p[i] = s2[i - s1count]);
+	p[sizeBuffer] = '\0';
+	return (p);
 }
