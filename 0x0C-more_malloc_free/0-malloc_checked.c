55d3658e4150e4cecda4b2478a060ae99339e2f0
Davida149 <adegundedavid17@gmail.com>
2022-04-07 23:40:29 +0100

    0-malloc_checked.c

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -2,15 +2,16 @@
 #include <stdlib.h>
 #include <stdio.h>
 /**
+ *malloc_checked - allocates memory using malloc
+ * @b: size of buffer
  *
  *
- *
- *
- *
+ *Return: pointer to buffer
  */
 void *malloc_checked(unsigned int b)
 {
-	void *ptr = malloc(sizeof (*ptr) * b);
+	void *ptr = malloc(sizeof(*ptr) * b);
+
 	if (ptr != NULL)
 	{
 		return (ptr);
