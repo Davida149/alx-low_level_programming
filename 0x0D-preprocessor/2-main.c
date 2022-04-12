321d62ab73bcaa5fb3a48a1ae5ce01fb0620f424
Davida149 <adegundedavid17@gmail.com>
2022-04-13 00:28:30 +0100

    2-main.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+/**
+ *main - prints name of file this was compiled from
+ *@void: accepts nothing
+ *return: void
+ */
+
+int main(void)
+{
+	printf("%s", _FILE_);
+
+	return (void);
+}
