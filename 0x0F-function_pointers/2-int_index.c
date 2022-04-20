6ec017fa3f89dce38d831f38b59eb4b68e429b13
Davida149 <adegundedavid17@gmail.com>
2022-04-20 07:16:47 -0700

    2-int_index.c

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,25 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+/**
+ *int_index - runs through the array and use the function pointer
+ *@array: pointer to array where elements are
+ *@size: number of elements in array
+ *@cmp: function pointer
+ *Return: -1 if 0 else ind
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i, idx = 0;
+	/*setting the requirements*/
+	if (size <= 0)
+		return (-1);
+	if (size && cmp)
+		/*go through the loop and use the function pointer*/
+		for (i = 0; i < size; i++)
+		{
+			idx = cmp(array[i]);/*adding the return value*/
+			if (idx == 1)/*checks for the first true element*/
+				return (i);
+		}
+	return (-1);/*if program comes here, it means functions are not true*/
+}
