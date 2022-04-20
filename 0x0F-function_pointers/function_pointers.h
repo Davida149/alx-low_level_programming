6ef37449c38153f2747898fe419a297431b11bd4
Davida149 <adegundedavid17@gmail.com>
2022-04-20 05:46:33 -0700

    0-print_name.c

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,12 +1,5 @@
-<<<<<<< HEAD
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
 #include <stdlib.h>
 void print_name(char *name, void (*f)(char *));
 #endif 
-=======
-#ifndef MAIN_H
-#define MAIN_H
-void print_name(char *name, void (*f)(char *));
-#endif
->>>>>>> 6a4b3b059268e01a72196151093a95fec25bc678
